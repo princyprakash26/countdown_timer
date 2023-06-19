90ca879b88aedb56b8ae17f8f68353848816be74
princyprakash26 <princyprakash26/pythoncode>
2023-06-19 08:13:59 +0530

    commit

---
+++ project_04/main.py
@@ -0,0 +1,15 @@
+#countdown timer
+
+import time
+
+def countdown(t):
+    while t:
+        mins,secs = divmod(t, 60)
+        timer='{:02d}: {:02d}'.format(mins,secs)
+        print(timer, end='\r')
+        time.sleep(1)
+        t -= 1
+    print('timer completed')
+t = input('Enter time in second')
+countdown(int(t))
+
